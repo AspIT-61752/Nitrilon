3917ef8f456419d39cb463020dd1bd8f1147e433
Tobsi <61752@edu.campusvejle.dk>
2024-04-09 08:06:47 -0500

    added EventRatings and a controller

---
+++ src/Nitrilon/Nitrilon.Api/Controllers/EventRatingsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nitrilon.Api.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Nitrilon.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventRatingsController : ControllerBase
+    {
+        // GET: api/<EventRatingsController>
+        [HttpGet]
+        public IEnumerable<EventRatings> Get()
+        {
+            // Todo: Implement an endpoint to get all event ratings.
+            return Enumerable.Range(1, 2).Select(index => new EventRatings()).ToArray();
+        }
+
+        // GET api/<EventRatingsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EventRatingsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EventRatingsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EventRatingsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Nitrilon/Nitrilon.Api/Models/EventRatings.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;        // Using this namespace to use Key attribute
+using System.ComponentModel.DataAnnotations.Schema; // Using this namespace to use ForeignKey attribute
+
+namespace Nitrilon.Api.Models
+{
+    public class EventRatings
+    {
+        // int? is a nullable type in C#.                       It is used to represent a variable that can be assigned null.
+        // int! is a non-nullable type in C#.                   It is used to represent a variable that cannot be assigned null.
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EventRatingId { get; }
+
+        [ForeignKey("Event")]
+        public int EventId { get; }
+
+        [ForeignKey("Rating")]
+        public int RatingId { get; }
+
+        // Navigation properties. 
+        //public virtual Event Event { get; set; }
+        //public virtual Rating Rating { get; set; }
+
+    }
+}
