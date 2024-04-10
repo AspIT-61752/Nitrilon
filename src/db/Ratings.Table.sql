ad4bc807c8e3b950b344df19155caa571fcdf1e3
Tobsi <144097339+AspIT-61752@users.noreply.github.com>
2024-04-10 14:37:10 +0200

    Update Ratings.Table.sql

--- src/db/Ratings.Table.sql
+++ src/db/Ratings.Table.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Ratings]
 (
 	[RatingId] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [Value] TINYINT NOT NULL, 
+    [RatingValue] TINYINT NOT NULL, 
     [Description] NVARCHAR(MAX) NOT NULL
 )
