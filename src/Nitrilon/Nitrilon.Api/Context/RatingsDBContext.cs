d38c7c622286d8d77bd75a1e99a0211c112f3575
AspIT-61752 <61752@edu.campusvejle.dk>
2024-04-10 14:56:33 +0200

    started working on the DB Context

---
+++ src/Nitrilon/Nitrilon.Api/Context/RatingsDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nitrilon.Api.Models;
+
+namespace Nitrilon.Api.Context
+{
+    // DbContext is a class provided by Entity Framework Core that represents a session with the database, allowing us to query and save instances of our entities.
+    // For mroe info: https://learn.microsoft.com/en-us/dotnet/api/system.data.entity.dbcontext?view=entity-framework-6.2.0
+    public class RatingsDBContext : DbContext
+    {
+        public DbSet<Ratings> Ratings { get; set; }
+        public DbSet<Events> Events { get; set; }
+    }
+}
