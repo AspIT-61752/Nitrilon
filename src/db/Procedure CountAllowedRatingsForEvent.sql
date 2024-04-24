9703de018ab1319e64d01ce27bca21b41da63799
AspIT-61752 <61752@edu.campusvejle.dk>
2024-04-24 11:05:47 +0200

    [Cleaning] removed unused and unnecessary files from the project

---
+++ src/db/Procedure CountAllowedRatingsForEvent.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE CountAllowedRatingsForEvent
+    @EventId INT
+AS
+BEGIN
+    DECLARE @BadCount INT, @AverageCount INT, @GoodCount INT;
+
+    SELECT @BadCount = COUNT(CASE WHEN RatingId = 1 THEN 1 ELSE NULL END),
+           @AverageCount = COUNT(CASE WHEN RatingId = 2 THEN 1 ELSE NULL END),
+           @GoodCount = COUNT(CASE WHEN RatingId = 3 THEN 1 ELSE NULL END)
+    FROM EventRatings
+    WHERE EventId = @EventId;
+
+    SELECT @BadCount AS RatingBad,
+           @AverageCount AS RatingAverage,
+           @GoodCount AS RatingGood;
+END
