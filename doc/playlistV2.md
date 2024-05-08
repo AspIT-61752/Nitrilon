ee9de0a390837d8bdc86561ee4f20a0bc5b972c3
Tobsi <61752@edu.campusvejle.dk>
2024-05-07 23:37:51 -0500

    Updated language on UI and worked on the playlist

--- doc/playlistV2.md
+++ doc/playlistV2.md
@@ -7,16 +7,20 @@
 
 - Beskriv den valgte systemudviklingsmetode, som er agil og iterativ.
 - Forklar de forskellige faser: Inception, Elaboration, Construction og Testing.
+  - Inception: Kunden giver os en forklaring af hvad de vil have. Vi omskriver det til krav specifikationer (**IKKE Design valg**).
+  - Elaboration: Her finder vi alle de entities der kan være, hvorefter vi filtrere dem en efter en (Vi brugte Tillægsord og navneord. Kan ikke huske hvad det var)
+  - Construction: Her går vi i gang med at løse opgaven. Så få database, klasser og filer op og køre ud fra den analyse og de diagrammer vi lavede tidligere.
+  - Testing: Det har vi ikke været så meget inde under, ud over SWAGGER og når vi skulle teste en ny feature eller teste UI.
 - **Inception:** Identificer krav gennem interviews, workshops osv. Et godt krav er klart, specifikt, målbart og realistisk.
 - **Elaboration:** Fra identificerede krav til en mere detaljeret forståelse og planlægning af systemet. Brug af værktøjer som analyse- og design-diagrammer.
 - Vis projektstyring på GitHub og hvordan issues håndteres.
 
 - Fandt ud af vi mangler at kunne tilføje et nyt event
+  - Kunden mangler en måde at kunne tilføje, redigere og fjerne events.
 
 # UI/UX og Funktionaliteter:
 
 - Vis eksempler på UI og hvordan det interagerer med de bagvedliggende systemkomponenter.
-  - Jeg tror vi har overset at de skulle kunne oprette et event.
 - Gennemgå de funktionelle krav og hvordan de implementeres i UI'et.
 
 # Construction:
@@ -27,6 +31,7 @@
 - Gennemgå API-design, herunder endpoints og rollen af IIS.
 
 - IIS er som en vært for hjemmesider på internettet. Det tager imod anmodninger fra folk, der vil besøge disse hjemmesider, og viser dem det rigtige indhold. Det sørger for, at internettet fungerer godt, ligesom en vært sørger for, at gæsterne har det godt til en fest.
+  - Tog: Sørger for at dem der skal til Vejle fra Fredericia kan stige på ved spor 3, og komme til vejle kl 8:28
 - Forklar Data Access gennem Repository designmønster.
 - Diskuter OOP-principper som indkapsling, komposition/aggregation og arv og hvordan de anvendes i systemet.
 
--- src/Nitrilon/Nitrilon.Web/Admin/JS/script.js
+++ src/Nitrilon/Nitrilon.Web/Admin/JS/script.js
@@ -1,280 +1,280 @@
 const memberURL = "https://localhost:7201/api/Member";
 
 fetch(memberURL)
-  .then((response) => response.json())
-  .then((data) => {
-    // Get the <section> element
-    let memberList = document.querySelector("#memberList");
+	.then((response) => response.json())
+	.then((data) => {
+		// Get the <section> element
+		let memberList = document.querySelector("#memberList");
 
-    // Loop through the events and create <li> elements
-    data.forEach((Member) => {
-      let li = document.createElement("li");
-      let Name = document.createElement("h2");
-      let MembershipStatus = document.createElement("p");
-      let CreationDate = document.createElement("p");
-      let PhoneNumb = document.createElement("p");
-      let EmailText = document.createElement("p");
-      let buttonContainer = document.createElement("div");
-      let del = document.createElement("button");
-      let edit = document.createElement("button");
+		// Loop through the events and create <li> elements
+		data.forEach((Member) => {
+			let li = document.createElement("li");
+			let Name = document.createElement("h2");
+			let MembershipStatus = document.createElement("p");
+			let CreationDate = document.createElement("p");
+			let PhoneNumb = document.createElement("p");
+			let EmailText = document.createElement("p");
+			let buttonContainer = document.createElement("div");
+			let del = document.createElement("button");
+			let edit = document.createElement("button");
 
-      console.log(Member);
+			console.log(Member);
 
-      Name.textContent = Member.fullName;
+			Name.textContent = Member.fullName;
 
-      if (Member.membership.membershipId == 2) {
-        MembershipStatus.textContent = "Aktiv";
-      } else if (Member.membership.membershipId == 1) {
-        MembershipStatus.textContent = "Passiv";
-      }
+			if (Member.membership.membershipId == 2) {
+				MembershipStatus.textContent = "Aktiv";
+			} else if (Member.membership.membershipId == 1) {
+				MembershipStatus.textContent = "Passiv";
+			}
 
-      let date = Member.joinDate.split("T")[0];
-      CreationDate.textContent = date;
-      CreationDate.id = "date";
+			let date = Member.joinDate.split("T")[0];
+			CreationDate.textContent = date;
+			CreationDate.id = "date";
 
-      PhoneNumb.textContent = Member.phoneNumber;
-      EmailText.textContent = Member.email;
-      EmailText.id = "email";
+			PhoneNumb.textContent = Member.phoneNumber;
+			EmailText.textContent = Member.email;
+			EmailText.id = "email";
 
-      del.textContent = "Fjern";
-      del.id = "delete";
+			del.textContent = "Fjern";
+			del.id = "delete";
 
-      del.addEventListener("click", function () {
-        // Display confirmation pop-up
-        if (confirm("Er du sikker på at du vil fjerne dette medlem?")) {
-          console.log(Member);
-          console.log(JSON.stringify(Member.memberId));
-          fetch(memberURL + `?id=${Member.memberId}`, {
-            method: "DELETE",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
+			del.addEventListener("click", function () {
+				// Display confirmation pop-up
+				if (confirm("Er du sikker på at du vil fjerne dette medlem?")) {
+					console.log(Member);
+					console.log(JSON.stringify(Member.memberId));
+					fetch(memberURL + `?id=${Member.memberId}`, {
+						method: "DELETE",
+						headers: {
+							"Content-Type": "application/json",
+						},
+					});
 
-          // Remove the member from the list
-          li.remove();
-        }
-      });
+					// Remove the member from the list
+					li.remove();
+				}
+			});
 
-      edit.textContent = "Rediger";
-      edit.id = "edit";
-      edit.addEventListener("click", function () {
-        // Create a modal element
-        let modal = document.createElement("div");
-        modal.classList.add("modal");
+			edit.textContent = "Rediger";
+			edit.id = "edit";
+			edit.addEventListener("click", function () {
+				// Create a modal element
+				let modal = document.createElement("div");
+				modal.classList.add("modal");
 
-        // Create form elements for editing member data
-        let form = document.createElement("form");
+				// Create form elements for editing member data
+				let form = document.createElement("form");
 
-        // Create input elements for editing member data
-        let nameLabel = document.createElement("label");
-        nameLabel.textContent = "Name:";
-        let nameInput = document.createElement("input");
-        nameInput.type = "text";
-        nameInput.value = Member.fullName;
+				// Create input elements for editing member data
+				let nameLabel = document.createElement("label");
+				nameLabel.textContent = "Navn:";
+				let nameInput = document.createElement("input");
+				nameInput.type = "text";
+				nameInput.value = Member.fullName;
 
-        let membershipLabel = document.createElement("label");
-        membershipLabel.textContent = "Membership:";
-        let membershipSelect = document.createElement("select");
-        let activeOption = document.createElement("option");
-        activeOption.value = 2;
-        activeOption.textContent = "Aktiv";
-        let passiveOption = document.createElement("option");
-        passiveOption.value = 1;
-        passiveOption.textContent = "Passiv";
-        membershipSelect.appendChild(activeOption);
-        membershipSelect.appendChild(passiveOption);
-        membershipSelect.value = Member.membership.membershipId;
+				let membershipLabel = document.createElement("label");
+				membershipLabel.textContent = "Medlemskab:";
+				let membershipSelect = document.createElement("select");
+				let activeOption = document.createElement("option");
+				activeOption.value = 2;
+				activeOption.textContent = "Aktiv";
+				let passiveOption = document.createElement("option");
+				passiveOption.value = 1;
+				passiveOption.textContent = "Passiv";
+				membershipSelect.appendChild(activeOption);
+				membershipSelect.appendChild(passiveOption);
+				membershipSelect.value = Member.membership.membershipId;
 
-        let joinDateLabel = document.createElement("label");
-        joinDateLabel.textContent = "Join Date:";
-        let joinDateInput = document.createElement("input");
-        joinDateInput.type = "date";
-        joinDateInput.value = Member.joinDate.split("T")[0];
+				let joinDateLabel = document.createElement("label");
+				joinDateLabel.textContent = "Oprettelsesdato:";
+				let joinDateInput = document.createElement("input");
+				joinDateInput.type = "date";
+				joinDateInput.value = Member.joinDate.split("T")[0];
 
-        let phoneNumberLabel = document.createElement("label");
-        phoneNumberLabel.textContent = "Phone Number:";
-        let phoneNumberInput = document.createElement("input");
-        phoneNumberInput.type = "text";
-        phoneNumberInput.value = Member.phoneNumber;
+				let phoneNumberLabel = document.createElement("label");
+				phoneNumberLabel.textContent = "Telefon:";
+				let phoneNumberInput = document.createElement("input");
+				phoneNumberInput.type = "text";
+				phoneNumberInput.value = Member.phoneNumber;
 
-        let emailLabel = document.createElement("label");
-        emailLabel.textContent = "Email Address:";
-        let emailInput = document.createElement("input");
-        emailInput.type = "email";
-        emailInput.value = Member.email;
+				let emailLabel = document.createElement("label");
+				emailLabel.textContent = "Email:";
+				let emailInput = document.createElement("input");
+				emailInput.type = "email";
+				emailInput.value = Member.email;
 
-        let saveButton = document.createElement("button");
-        saveButton.textContent = "Save";
-        saveButton.addEventListener("click", function () {
-          // Update member data
-          Member.fullName = nameInput.value;
-          Member.membership.membershipId = parseInt(membershipSelect.value);
-          Member.joinDate = joinDateInput.value;
-          Member.phoneNumber = phoneNumberInput.value;
-          Member.email = emailInput.value;
+				let saveButton = document.createElement("button");
+				saveButton.textContent = "Gem";
+				saveButton.addEventListener("click", function () {
+					// Update member data
+					Member.fullName = nameInput.value;
+					Member.membership.membershipId = parseInt(membershipSelect.value);
+					Member.joinDate = joinDateInput.value;
+					Member.phoneNumber = phoneNumberInput.value;
+					Member.email = emailInput.value;
 
-          console.log(Member);
+					console.log(Member);
 
-          // Update the member in the database
-          fetch(memberURL, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(Member),
-          });
+					// Update the member in the database
+					fetch(memberURL, {
+						method: "PUT",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(Member),
+					});
 
-          // Close the modal
-          modal.remove();
-        });
+					// Close the modal
+					modal.remove();
+				});
 
-        let cancelButton = document.createElement("button");
-        cancelButton.textContent = "Cancel";
-        cancelButton.addEventListener("click", function () {
-          // Close the modal without saving changes
-          modal.remove();
-        });
+				let cancelButton = document.createElement("button");
+				cancelButton.textContent = "Annuller";
+				cancelButton.addEventListener("click", function () {
+					// Close the modal without saving changes
+					modal.remove();
+				});
 
-        // Append input elements to the form
-        form.appendChild(nameLabel);
-        form.appendChild(nameInput);
-        form.appendChild(membershipLabel);
-        form.appendChild(membershipSelect);
-        form.appendChild(joinDateLabel);
-        form.appendChild(joinDateInput);
-        form.appendChild(phoneNumberLabel);
-        form.appendChild(phoneNumberInput);
-        form.appendChild(emailLabel);
-        form.appendChild(emailInput);
-        form.appendChild(saveButton);
-        form.appendChild(cancelButton);
+				// Append input elements to the form
+				form.appendChild(nameLabel);
+				form.appendChild(nameInput);
+				form.appendChild(membershipLabel);
+				form.appendChild(membershipSelect);
+				form.appendChild(joinDateLabel);
+				form.appendChild(joinDateInput);
+				form.appendChild(phoneNumberLabel);
+				form.appendChild(phoneNumberInput);
+				form.appendChild(emailLabel);
+				form.appendChild(emailInput);
+				form.appendChild(saveButton);
+				form.appendChild(cancelButton);
 
-        // Append the form to the modal
-        modal.appendChild(form);
+				// Append the form to the modal
+				modal.appendChild(form);
 
-        // Append the modal to the document body
-        document.body.appendChild(modal);
-      });
+				// Append the modal to the document body
+				document.body.appendChild(modal);
+			});
 
-      li.appendChild(Name);
-      li.appendChild(EmailText);
-      li.appendChild(PhoneNumb);
-      li.appendChild(CreationDate);
-      li.appendChild(MembershipStatus);
-      buttonContainer.appendChild(del);
-      buttonContainer.appendChild(edit);
-      li.appendChild(buttonContainer);
+			li.appendChild(Name);
+			li.appendChild(EmailText);
+			li.appendChild(PhoneNumb);
+			li.appendChild(CreationDate);
+			li.appendChild(MembershipStatus);
+			buttonContainer.appendChild(del);
+			buttonContainer.appendChild(edit);
+			li.appendChild(buttonContainer);
 
-      memberList.appendChild(li);
-    });
-  })
-  .catch((error) => {
-    console.error("Error fetching Members:", error);
-  });
+			memberList.appendChild(li);
+		});
+	})
+	.catch((error) => {
+		console.error("Error fetching Members:", error);
+	});
 
 let addMemberButton = document.querySelector("#add");
 addMemberButton.addEventListener("click", function () {
-  // Create a modal element
-  let modal = document.createElement("div");
-  modal.classList.add("modal");
+	// Create a modal element
+	let modal = document.createElement("div");
+	modal.classList.add("modal");
 
-  // Create form elements for adding a new member
-  let form = document.createElement("form");
+	// Create form elements for adding a new member
+	let form = document.createElement("form");
 
-  // Create input elements for adding a new member
-  let nameLabel = document.createElement("label");
-  nameLabel.textContent = "Name:";
-  let nameInput = document.createElement("input");
-  nameInput.type = "text";
+	// Create input elements for adding a new member
+	let nameLabel = document.createElement("label");
+	nameLabel.textContent = "Navn:";
+	let nameInput = document.createElement("input");
+	nameInput.type = "text";
 
-  let membershipLabel = document.createElement("label");
-  membershipLabel.textContent = "Membership:";
-  let membershipSelect = document.createElement("select");
-  let activeOption = document.createElement("option");
-  activeOption.value = 2;
-  activeOption.textContent = "Aktiv";
-  let passiveOption = document.createElement("option");
-  passiveOption.value = 1;
-  passiveOption.textContent = "Passiv";
-  membershipSelect.appendChild(activeOption);
-  membershipSelect.appendChild(passiveOption);
+	let membershipLabel = document.createElement("label");
+	membershipLabel.textContent = "Medlemskab:";
+	let membershipSelect = document.createElement("select");
+	let activeOption = document.createElement("option");
+	activeOption.value = 2;
+	activeOption.textContent = "Aktiv";
+	let passiveOption = document.createElement("option");
+	passiveOption.value = 1;
+	passiveOption.textContent = "Passiv";
+	membershipSelect.appendChild(activeOption);
+	membershipSelect.appendChild(passiveOption);
 
-  let joinDateLabel = document.createElement("label");
-  joinDateLabel.textContent = "Join Date:";
-  let joinDateInput = document.createElement("input");
-  joinDateInput.type = "date";
+	let joinDateLabel = document.createElement("label");
+	joinDateLabel.textContent = "Oprettelsesdato:";
+	let joinDateInput = document.createElement("input");
+	joinDateInput.type = "date";
 
-  let phoneNumberLabel = document.createElement("label");
-  phoneNumberLabel.textContent = "Phone Number:";
-  let phoneNumberInput = document.createElement("input");
-  phoneNumberInput.type = "text";
+	let phoneNumberLabel = document.createElement("label");
+	phoneNumberLabel.textContent = "Telefon:";
+	let phoneNumberInput = document.createElement("input");
+	phoneNumberInput.type = "text";
 
-  let emailLabel = document.createElement("label");
-  emailLabel.textContent = "Email Address:";
-  let emailInput = document.createElement("input");
-  emailInput.type = "email";
+	let emailLabel = document.createElement("label");
+	emailLabel.textContent = "Email:";
+	let emailInput = document.createElement("input");
+	emailInput.type = "email";
 
-  let saveButton = document.createElement("button");
-  saveButton.textContent = "Add";
-  saveButton.addEventListener("click", function () {
-    // Create a new member
-    let membershipVar = {
-      membership: {
-        membershipId: parseInt(membershipSelect.value),
-        name: "string",
-        description: "string",
-      },
-    };
+	let saveButton = document.createElement("button");
+	saveButton.textContent = "Tilføj";
+	saveButton.addEventListener("click", function () {
+		// Create a new member
+		let membershipVar = {
+			membership: {
+				membershipId: parseInt(membershipSelect.value),
+				name: "string",
+				description: "string",
+			},
+		};
 
-    let member = {
-      fullName: nameInput.value,
-      membership: membershipVar.membership, // Access the inner object directly
-      joinDate: joinDateInput.value,
-      phoneNumber: phoneNumberInput.value,
-      email: emailInput.value,
-    };
+		let member = {
+			fullName: nameInput.value,
+			membership: membershipVar.membership, // Access the inner object directly
+			joinDate: joinDateInput.value,
+			phoneNumber: phoneNumberInput.value,
+			email: emailInput.value,
+		};
 
-    console.log(membershipSelect.value);
-    console.log(member);
+		console.log(membershipSelect.value);
+		console.log(member);
 
-    // Add the new member to the database
-    fetch(memberURL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(member),
-    });
+		// Add the new member to the database
+		fetch(memberURL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(member),
+		});
 
-    // Close the modal
-    modal.remove();
-  });
+		// Close the modal
+		modal.remove();
+	});
 
-  let cancelButton = document.createElement("button");
-  cancelButton.textContent = "Cancel";
-  cancelButton.addEventListener("click", function () {
-    // Close the modal without saving the new member
-    modal.remove();
-  });
+	let cancelButton = document.createElement("button");
+	cancelButton.textContent = "Annuller";
+	cancelButton.addEventListener("click", function () {
+		// Close the modal without saving the new member
+		modal.remove();
+	});
 
-  // Append input elements to the form
-  form.appendChild(nameLabel);
-  form.appendChild(nameInput);
-  form.appendChild(membershipLabel);
-  form.appendChild(membershipSelect);
-  form.appendChild(joinDateLabel);
-  form.appendChild(joinDateInput);
-  form.appendChild(phoneNumberLabel);
-  form.appendChild(phoneNumberInput);
-  form.appendChild(emailLabel);
-  form.appendChild(emailInput);
-  form.appendChild(saveButton);
-  form.appendChild(cancelButton);
+	// Append input elements to the form
+	form.appendChild(nameLabel);
+	form.appendChild(nameInput);
+	form.appendChild(membershipLabel);
+	form.appendChild(membershipSelect);
+	form.appendChild(joinDateLabel);
+	form.appendChild(joinDateInput);
+	form.appendChild(phoneNumberLabel);
+	form.appendChild(phoneNumberInput);
+	form.appendChild(emailLabel);
+	form.appendChild(emailInput);
+	form.appendChild(saveButton);
+	form.appendChild(cancelButton);
 
-  // Append the form to the modal
-  modal.appendChild(form);
+	// Append the form to the modal
+	modal.appendChild(form);
 
-  // Append the modal to the document body
-  document.body.appendChild(modal);
+	// Append the modal to the document body
+	document.body.appendChild(modal);
 });
